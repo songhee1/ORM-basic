8bdcc6d6ef78f88707d7ccd055517cc8d09394a4
songhee1 <ghenrhkwk88@naver.com>
2024-11-18 17:05:11 +0900

    try-catch문으로 db 커넥션 접속 및 해제

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -9,9 +9,20 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // db 트랜잭션 시작
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("사용자2");
+            em.persist(member);
+            tx.commit();
+        }catch(Exception e) {
+            tx.rollback();
+        }finally{
+            em.close();
+        }
 
-
-        em.close();
         emf.close();
     }
 }
