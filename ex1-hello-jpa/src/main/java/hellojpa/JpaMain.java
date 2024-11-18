2e6e7f1b29c6a44e53690774c40d1cb3686d18f8
songhee1 <ghenrhkwk88@naver.com>
2024-11-18 17:15:56 +0900

    entityManager과 트랜잭션접

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -7,15 +7,27 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        //EntityManager : 고객 요청이 올때마다 생성해 사용 - 공유x
         EntityManager em = emf.createEntityManager();
         //code
         EntityTransaction tx = em.getTransaction();
         tx.begin(); // db 트랜잭션 시작
         try{
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("사용자2");
-            em.persist(member);
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = "+findMember.getId());
+            System.out.println("findMember.name = "+findMember.getName());
+
+            Member findMember2 = em.find(Member.class, 2L);
+            em.remove(findMember2); // delete 쿼리 및 삭제
+
+            findMember.setName("HelloJPA");
+            //em.persist로 저장하지 않아도 됨
+            /*
+            * 이유: JPA를 통해서 엔티티를 가져오면 JPA에서 엔티티를 관리하게 됨
+            * 변경이 되었는지 등 트랜잭션을 체크함.
+            * 변경된 부분이 있다면 UPDATE 쿼리 날리고 transaction commit 날림
+            * JPA의 모든 DATA 변경은 트랜잭션 안에서 실행
+            * */
             tx.commit();
         }catch(Exception e) {
             tx.rollback();
