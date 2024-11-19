8e5f8c9a6ced1994dfd2a20df0f325e1202a0570
songhee1 <ghenrhkwk88@naver.com>
2024-11-19 23:14:46 +0900

    양방향 연관관계의 주인2-양방향 객체 세팅

--- jpashop/src/main/java/jpabook.jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook.jpashop/JpaMain.java
@@ -15,18 +15,32 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Member member = new Member();
-            member.setName("member1");
-            em.persist(member);
-
             Team team = new Team();
             team.setName("TeamA");
-            //team.getMembers().add(member); // 읽기 전용->외래키값 세팅x
-            member.setTeam(team); // 연관관계 주인->외래키값 세팅o
             em.persist(team);
 
-            em.flush();
-            em.clear();
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            //5. 객체지향적으로 양쪽 모두 세팅 필요
+            team.getMembers().add(member);
+            /*
+            * 1.
+            * (필요X) team.getMembers().add(member)
+            * -> (문제X) JPA 동작시 TEAM_ID로 members 세팅되기 때문
+            * */
+
+            //2. 아래 두 줄이 없다면?
+            /*em.flush();
+            em.clear();*/
+
+            Team findTeam = em.find(Team.class, team.getId()); //3. 1차 캐시 : member 없는 채로 team 객체 저장
+            List<Member> members = findTeam.getMembers();
+            for (Member m : members) {
+                System.out.println("m = "+m.getName()); // 4. 출력x
+            }
 
             tx.commit();
         }catch(Exception e){
