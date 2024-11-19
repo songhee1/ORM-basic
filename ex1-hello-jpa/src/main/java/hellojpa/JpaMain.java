46341bc8fa9a3282d93eab4ea915c734afbcdd87
songhee1 <ghenrhkwk88@naver.com>
2024-11-19 15:43:52 +0900

    기본키매핑-SEQUENCE allocationSize

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,10 +15,19 @@ public class JpaMain {
         tx.begin(); // db 트랜잭션 시작 - 데이터의 모든 변경은 트랜잭션 안에서 일어나야 함
         try{
             Member member = new Member();
+            Member member2 = new Member();
+            Member member3 = new Member();
             System.out.println("===구분선===");
-            em.persist(member); // IDENTITY전략-INSERT 쿼리 즉시 발생
+            em.persist(member); // 시퀀스ID값 생성-1, 시퀀스ID값 생성-51
+            em.persist(member2);
+            em.persist(member3);
             System.out.println("===구분선===");
-            tx.commit(); // UPDATE 쿼리 발생x
+
+            System.out.println("member.id = "+member.getId());  // 1,51  |1
+            System.out.println("member2.id = "+member2.getId());// MEMORY|2
+            System.out.println("member3.id = "+member3.getId());// MEMORY|3
+
+            tx.commit(); // INSERT 쿼리
         }catch(Exception e) {
             tx.rollback();
         }finally{
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,19 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
+@SequenceGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+    initialValue = 1, allocationSize = 50)
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    public Long getId() {
-        return id;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
 
     public Member(){
 
     }
 
+    public Long getId() {
+        return id;
+    }
 }
