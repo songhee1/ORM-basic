c4306b5963312d0cc99c48ff38aee763a69fb0cb
songhee1 <ghenrhkwk88@naver.com>
2024-11-19 14:58:17 +0900

    기본키매핑-SEQUENCE

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import java.lang.reflect.Member;
 import java.util.List;
 
 public class JpaMain {
@@ -14,18 +15,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); // db 트랜잭션 시작 - 데이터의 모든 변경은 트랜잭션 안에서 일어나야 함
         try{
-            Member member = new Member();
-            member.setId(1L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.USER);
 
-            Member member2 = new Member();
-            member2.setId(2L);
-            member2.setUsername("B");
-            member2.setRoleType(RoleType.ADMIN);
-
-            em.persist(member);
-            em.persist(member2);
             tx.commit(); // UPDATE 쿼리 발생x
         }catch(Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,59 +1,26 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.persistence.Transient;
-import java.util.Date;
+import jakarta.persistence.TableGenerator;
 
 @Entity
+@TableGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    table = "MY_SEQUENCES",
+    pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    @Column(name = "name", updatable = false)
-    private String username;
-    private Integer age;
-    @Enumerated(EnumType.ORDINAL)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
 
-    @Transient // 메모리에서만 사용, db컬럼x
-    private int temp;
-
-    //Getter, Setter
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
     }
 }
