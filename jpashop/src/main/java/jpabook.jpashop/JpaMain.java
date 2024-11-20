60b2893fdd7a4daae435193d22d16c8cba8cd09b
songhee1 <ghenrhkwk88@naver.com>
2024-11-20 17:59:45 +0900

    다양한 연관관계 매핑

--- jpashop/src/main/java/jpabook.jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook.jpashop/JpaMain.java
@@ -4,12 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import java.util.List;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.MemberTest;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.Team;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -18,14 +12,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            MemberTest member = new MemberTest();
-            member.setUsername("member1");
-            em.persist(member);
 
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member); // Member테이블 UPDATE 쿼리 발생, 다른 table UPDATE(권장X)
-            em.persist(team);
             tx.commit();
         }catch(Exception e){
             tx.rollback();
--- jpashop/src/main/java/jpabook.jpashop/domain/Item.java
+++ jpashop/src/main/java/jpabook.jpashop/domain/Item.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Item {
@@ -13,6 +17,9 @@ public class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
     public Long getId() {
         return id;
     }
