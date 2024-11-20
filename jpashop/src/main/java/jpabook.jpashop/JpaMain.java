8881072b461b41db21ea39f7d68c81f1910c5cbc
songhee1 <ghenrhkwk88@naver.com>
2024-11-21 00:21:26 +0900

    프록시-주의점

--- jpashop/src/main/java/jpabook.jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook.jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jdk.swing.interop.SwingInterOpUtils;
 import jpabook.jpashop.domain.Member;
 
 public class JpaMain {
@@ -15,34 +16,18 @@ public class JpaMain {
         try{
             Member member1 = new Member();
             member1.setName("member1");
-            Member member2 = new Member();
-            member2.setName("member2");
-            Member member3 = new Member();
-            member3.setName("member3");
-
             em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
-            em.flush(); //INSERT 발생
+
+            em.flush();
             em.clear();
 
-            //1. 프록시 초기화
-//            Member findMember = em.find(Member.class, member.getId()); //SELECT 발생
-            Member findMember = em.getReference(Member.class, member1.getId()); //SELECT 발생x, 프록시 객체 반환
+            Member m1 = em.find(Member.class, member1.getId());
+            Member reference = em.getReference(Member.class, member1.getId());
+            System.out.println("m1 = " + m1.getClass());
+            System.out.println("reference = " + reference.getClass());
+            // 둘다 원본 반환 (프록시 객체 안나오는 이유 : 1. 성능상 이점X 2. JPA 메커니즘-트랜잭션 내 동시성 보장)
 
-            System.out.println("BEFORE findMember = "+findMember.getClass()); // ~Proxy 출력, target = null
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.username = " + findMember.getName()); // 초기화요청O, SELECT 발생
-            System.out.println("findMember.username = " + findMember.getName()); // 초기화요청X
-            System.out.println("AFTER findMember = "+findMember.getClass()); // ~Proxy 출력, target = 원본엔티티
-
-            //2. 타입 체크
-            Member m1 = em.find(Member.class, member2.getId());
-//            Member m2 = em.find(Member.class, member3.getId());
-            Member m3 = em.getReference(Member.class, member3.getId());
-//            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // true
-            System.out.println("m1 == m2 : " + (m1.getClass() == m3.getClass())); // false, instanceof 사용
-
+            System.out.println("m1 == reference" + (m1 == reference)); // true
             tx.commit();
         }catch(Exception e){
             tx.rollback();
