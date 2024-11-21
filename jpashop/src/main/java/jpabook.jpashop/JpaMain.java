69d3d946bfbf83ef5611d5ecfccf6964a4f4af39
songhee1 <ghenrhkwk88@naver.com>
2024-11-21 15:53:44 +0900

    영속성 전이 CASCADE

--- jpashop/src/main/java/jpabook.jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook.jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import com.sun.source.util.ParameterNameProvider;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -8,7 +9,9 @@ import jakarta.persistence.TypedQuery;
 import java.util.List;
 import jdk.swing.interop.SwingInterOpUtils;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.test.Child;
 import jpabook.jpashop.test.MemberTest;
+import jpabook.jpashop.test.Parent;
 import jpabook.jpashop.test.Team;
 import org.hibernate.Hibernate;
 import org.hibernate.jpa.internal.PersistenceUnitUtilImpl;
@@ -20,34 +23,16 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            MemberTest member1 = new MemberTest();
-            member1.setName("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            MemberTest member2 = new MemberTest();
-            member2.setName("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-            List<MemberTest> members = em.createQuery("select m from MemberTest m join fetch m.team", // LAZY loading + fetch join = Member, Team SELECT
-                MemberTest.class).getResultList();
-
-            for (MemberTest memberTest :members) {
-                System.out.println(memberTest.getTeam().getName()); // Team SELECT X
-            }
-
+            em.persist(parent); // CASCADE X :3개 persist 필요, CASCADE O : 1개 persist로 컬렉션 내용 모두 INSERT
+//            em.persist(child1);
+//            em.persist(child2);
             tx.commit();
         }catch(Exception e){
             tx.rollback();
---
+++ jpashop/src/main/java/jpabook/jpashop/test/Parent.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.test;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL) // cascade : 컬렉션 내용 모두 persist
+    List<Child> children = new ArrayList<>();
+
+    public void addChild(Child child){
+        children.add(child);
+        child.setParent(this);
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
