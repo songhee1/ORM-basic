d09c92ec629ae83f373dbbc454e50da6b638a186
songhee1 <ghenrhkwk88@naver.com>
2024-11-22 22:45:21 +0900

    JPQL기본 문법-getSingleResult

--- jpashop/src/main/java/jpabook/jpashop/ValueMain.java
+++ jpashop/src/main/java/jpabook/jpashop/ValueMain.java
@@ -4,13 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
-import java.util.List;
-import jpabook.jpashop.test.Address;
 import jpabook.jpashop.test.MemberTest;
 
 public class ValueMain {
@@ -22,17 +16,11 @@ public class ValueMain {
         tx.begin();
         try{
 
-            MemberTest member = new MemberTest();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
-
             TypedQuery<MemberTest> query = em.createQuery("select m from MemberTest m",
                 MemberTest.class);// 두번째 매개변수 = 엔티티 클래스명, 반화 클래스 명확, TypedQuery
-            Query query2 = em.createQuery("select m.username, m.age from MemberTest m");// 두번째 매개변수 = 엔티티 클래스명
 
-            List<MemberTest> resultList = query.getResultList(); // 컬렉션 반환
-            MemberTest result = query.getSingleResult();// 반환 1개, 그렇지않으면 Exception
+            MemberTest result = query.getSingleResult();// 반환 0개, Exception(반환 1개 보장시 사용)
+            // Spring Data Jpa : Optional, Null 반환, 예외x
             System.out.println("result = "+result);
 
 
