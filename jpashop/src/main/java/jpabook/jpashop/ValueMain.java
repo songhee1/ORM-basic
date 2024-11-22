2980e0b41611284dbc806a6b13a8036dc4b7615a
songhee1 <ghenrhkwk88@naver.com>
2024-11-22 23:51:27 +0900

    스칼라 타입 프로젝션 조회 반환 타입 종류

--- jpashop/src/main/java/jpabook/jpashop/ValueMain.java
+++ jpashop/src/main/java/jpabook/jpashop/ValueMain.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
 import java.util.List;
 import jpabook.jpashop.test.Address;
+import jpabook.jpashop.test.MemberDTO;
 import jpabook.jpashop.test.MemberTest;
 import jpabook.jpashop.test.Team;
 
@@ -27,26 +28,29 @@ public class ValueMain {
             em.flush();
             em.clear();
 
-            //엔티티 프로젝션
-            List<MemberTest> result = em.createQuery("select m from MemberTest m", // m : 엔티티, 엔티티 반환
-                MemberTest.class).getResultList(); // SELECT, 영속성 컨텍스트에서 관리
-
-            result.get(0).setAge(20); // UPDATE
-
-            //엔티티 프로젝션
-            List<Team> result2 = em.createQuery("select m.team from MemberTest m", Team.class) // 쿼리간단하나, join SELECT
-                .getResultList(); // 쿼리 예측 x(권장x)
-            List<Team> result3 = em.createQuery("select t from MemberTest m join m.team t", Team.class) // join SELECT
-                .getResultList(); // 쿼리 예측, 조인은 명시적으로 하는 것 권장
-
-            // 임베디드 타입 프로젝션
-            List<Address> result4 = em.createQuery("select o.address from OrderTest o",
-                    Address.class)
-                .getResultList();
-
             // 스칼라 타입 프로젝션
-            em.createQuery("select distinct m.username, m.age from MemberTest m") // join SELECT
+            List resultList = em.createQuery(
+                    "select distinct m.username, m.age from MemberTest m") // join SELECT
                 .getResultList();
+
+            // 1. Object 2. Object[]
+            Object o = resultList.get(0);
+            Object[] result = (Object[]) o;
+            System.out.println("username = "+result[0]); // username
+            System.out.println("age = "+result[1]); // age
+
+            //3. TypeQuery
+            List<Object[]> resultList2 = em.createQuery(
+                    "select distinct m.username, m.age from MemberTest m") // join SELECT
+                .getResultList(); // 타입 캐스팅
+
+            //4. new(권장)
+            List<MemberDTO> resultList1 = em.createQuery(
+                "select new jpabook.jpashop.test.MemberDTO(m.username, m.age) from MemberTest m",
+                MemberDTO.class).getResultList();
+            MemberDTO memberDTO = resultList1.get(0);
+            System.out.println("memberDTO.username = "+ memberDTO.getUsername());
+            System.out.println("memberDTO.age = "+ memberDTO.getAge());
 
 
             tx.commit();
