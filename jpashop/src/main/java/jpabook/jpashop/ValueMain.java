4371ac77e3ea0116329f2560e39d9a6fe8fc1c15
songhee1 <ghenrhkwk88@naver.com>
2024-11-23 11:42:40 +0900

    JPQL TYPE 엔티티 매핑

--- jpashop/src/main/java/jpabook/jpashop/ValueMain.java
+++ jpashop/src/main/java/jpabook/jpashop/ValueMain.java
@@ -4,13 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.TypedQuery;
 import java.util.List;
-import jpabook.jpashop.test.Address;
-import jpabook.jpashop.test.MemberDTO;
-import jpabook.jpashop.test.MemberTest;
-import jpabook.jpashop.test.MemberType;
-import jpabook.jpashop.test.Team;
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 
 public class ValueMain {
     public static void main(String[] args){
@@ -20,42 +16,17 @@ public class ValueMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-            MemberTest member = new MemberTest();
-            member.setUsername("memberA");
-            member.setType(MemberType.ADMIN);
-            member.setAge(10);
-
-            member.setTeam(team);
-            em.persist(member);
-
+            Book book = new Book();
+            book.setName("book");
+            em.persist(book);
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', TRUE, FALSE, true, false from MemberTest m "+
-                "where m.type = jpabook.jpashop.test.MemberType.ADMIN"; // 패키지명 기입
-
-            String query2 = "select m.username, 'HELLO', TRUE, FALSE, true, false from MemberTest m "+
-                "where m.type = :userType";
-            List<Object[]> result = em.createQuery(
-                    query)
+            List resultList = em.createQuery("select i from Item i where type(i) = Book", Item.class) // DTYPE = 'Book', 엔티티 타입 매핑
                 .getResultList();
-
-            List<Object[]> result2 = em.createQuery(
-                    query2).setParameter("userType", MemberType.ADMIN) // 파라미터 매핑 권장-이넘타입
-                .getResultList();
-
-
-            for (Object[] objects : result) {
-                System.out.println("objects = "+ objects[0]);
-                System.out.println("objects = "+ objects[1]);
-                System.out.println("objects = "+ objects[2]);
-            }
 
             tx.commit();
-        }catch(Exception e){
+        }catch(Exception e){ 
             tx.rollback();
             e.printStackTrace();
         }finally {
