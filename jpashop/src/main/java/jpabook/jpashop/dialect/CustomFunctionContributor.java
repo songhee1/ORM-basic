66abda5837d70693ba01dbe8fa1d7e518c4bd140
songhee1 <ghenrhkwk88@naver.com>
2024-11-23 12:20:26 +0900

    JPQL 함수-jpa표준, 사용자정의함수

---
+++ jpashop/src/main/java/jpabook/jpashop/dialect/CustomFunctionContributor.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.dialect;
+
+import org.hibernate.dialect.H2Dialect;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+
+public class MyH2Dialect extends H2Dialect {
+
+    public MyH2Dialect() {
+        registerFunction
+    }
+}
