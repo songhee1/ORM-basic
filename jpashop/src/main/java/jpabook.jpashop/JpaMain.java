7d4d7f7954462e1e355bdfdd4b365e528e2e2f47
songhee1 <ghenrhkwk88@naver.com>
2024-11-25 21:17:52 +0900

    페치조인 기본

--- jpashop/src/main/java/jpabook.jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook.jpashop/JpaMain.java
@@ -16,24 +16,36 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            em.persist(teamA);
+            Team teamB = new Team();
+            em.persist(teamB);
 
             MemberTest member1 = new MemberTest();
             MemberTest member2 = new MemberTest();
-            member1.setTeam(team);
-            member2.setTeam(team);
+            MemberTest member3 = new MemberTest();
+            member1.setTeam(teamA);
+            member1.setUsername("회원1");
+            member2.setUsername("회원2");
+            member3.setUsername("회원3");
+            member2.setTeam(teamA);
+            member3.setTeam(teamB);
             em.persist(member1);
             em.persist(member2);
+            em.persist(member3);
             em.flush();
             em.clear();
 
+            String query = "select m from MemberTest m"; // Team은 프록시 객체설정
+            List<MemberTest> result = em.createQuery(query, MemberTest.class).getResultList(); //select member
+            for (MemberTest member : result) { // teamA, teamB 각각 select
+                System.out.println("member="+member.getUsername()+", team name = " + member.getTeam().getName());
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차 캐시)
+                // 회원3, 팀B(SQL)
+                // 총 3번
 
-            // 컬렉션 값 연관경로
-            String query3 = "select m.username from Team t join t.members m"; // from절 명시적 내부조인(별칭), 탐색o
-            Collection result3 = em.createQuery(query3, Collection.class).getResultList(); // 묵시적 내부조인, 탐색x
-            for (Object o : result3) {
-                System.out.println("o = "+ o);
+                // 회원수 많을수록 N(SELECT TEAM)+1(SELECT MEMBER)쿼리, 즉시/지연로딩 마찬가지
             }
 
             tx.commit();
