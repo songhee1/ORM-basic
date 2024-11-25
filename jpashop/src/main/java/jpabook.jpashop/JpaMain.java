e14bf51e19c32c7a8daf16ab9ad8ac0bec466e9d
songhee1 <ghenrhkwk88@naver.com>
2024-11-25 22:17:58 +0900

    페치조인-일대다 조인 데이터 뻥튀기문제

--- jpashop/src/main/java/jpabook.jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook.jpashop/JpaMain.java
@@ -38,23 +38,15 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from MemberTest m join fetch m.team"; // Team 엔티티 초기화, 페치조인>지연로딩, 다대일 조인
-            String query2 = "select t from Team t join fetch t.members"; // 일대다 조인, data 뻥튀기(단점)
-            List<MemberTest> result = em.createQuery(query, MemberTest.class).getResultList(); //select member, team
-            List<Team> result2 = em.createQuery(query2, Team.class).getResultList();
-            for (MemberTest member : result) { // teamA, teamB 각각 select
-                System.out.println("member="+member.getUsername()+", team name = " + member.getTeam().getName());
-                // 회원1, 팀A(1차 캐시)
-                // 회원2, 팀A(1차 캐시)
-                // 회원3, 팀B(1차 캐시)
-                // 총 1번
-            }
+            String query = "select t from Team t join fetch t.members"; // 일대다 조인, data 뻥튀기(단점)->distinct(자동으로 됨)
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
 
-            for (Team team : result2) {
-                System.out.println("team = "+team.getName()+", team.getMembers().size() = "+ team.getMemberList().size()); // 중복발생
+            for (Team team : result) {
+                System.out.println("team = "+team.getName()+", team.getMembers().size() = "+ team.getMemberList().size()); // 중복제거(같은 식별자 가진 team 엔티티 제거)
                 for (MemberTest member : team.getMemberList()){
                     System.out.println("member = " +member);
                 }
+                System.out.println();
             }
 
             tx.commit();
